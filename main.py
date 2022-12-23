9e8fed4fbf5cea3579722a3029fe7f13c374aae8
Roshan Chhetri <98568962+Terabro@users.noreply.github.com>
2022-12-23 13:34:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+print("syntax is num operator num)
+
+
+a=int(input())
+x=input("Chose operand + - * / %)
+b=int(input())
+match x:
+  case '+': print(a+b)
+  case "-": print(a-b)
+  case "*": print(a*b)
+  case "/": print(a/b)
+  case "%": print(a%b)
+  case_ : print("invalid")
+  
+  
